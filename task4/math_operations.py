b75a15722dc76a744295627b0f9ffb14f7ba02b6
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2023-12-12 18:34:40 +0300

    math_operations.py

--- task4/math_operations.py
+++ task4/math_operations.py
@@ -1,1 +1,20 @@
-m
+from math_operations import basic_operations, power_operations, apply_operations
+# promots the user to enter the numbers
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+c = int(input("enter the amount of modulo:"))
+result_basic = basic_operations(a, b)
+print("Basic operations result:", result_basic)
+
+result_power = power_operations(a, b)
+print("Power Operation Result:", result_power)
+
+result_power_modulo = power_operations(a, b, modulo=c)
+print("Power Operation Result with modulo:", result_power_modulo)
+
+operations = [
+    (lambda x, y: x + y, (a, b))
+    , (lambda x, y: x * y, (a, b))
+]
+result_apply = apply_operations(operations)
+print("Apply operations result:", result_apply)
