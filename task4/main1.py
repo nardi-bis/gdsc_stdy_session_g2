c7b2e3ac59b89099e1d1dc2ce0afcb863911fe74
Nardos Bisrat <149002589+nardi-bis@users.noreply.github.com>
2023-12-12 18:45:32 +0300

    main1.py

--- task4/main1.py
+++ task4/main1.py
@@ -1,24 +1,17 @@
-import os
-import shutil
-import time
+from math_operations import basic_operations, power_operations, apply_operations
 
-def collect_and_move_files():
-    try:
-        current_directory = os.getcwd()
-        last_24hours_directory = os.path.join(current_directory, "last_24hours")
-
-        if not os.path.exisits(last_24hours_directory):
-            os.makedirs(last_24hours_directory)
-
-        files = [f for f in os.listdir(current_directory) if os.path.isfile(os.path.join(current_directory, f))]
-        current_time = time.time()
-
-        for file in files:
-            file_path = os.path.join(current_directory,file)
-            file_stat = os.stat(file_path)
-
-            if (current_time -  file_stat.st_mtime) < 24 * 3600 or (current_time - file_stat.st_ctime) < 24 * 3600:
-                shutil.move(file_path, os.path.join(last_24hours_directory, file))
-    except Exception as e:
-        print(f"Error: {e}")
-collect_and_move_files()
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+c = int(input("enter the amount of modulo:"))
+result_basic = basic_operations(a, b)
+print("Basic operations result:", result_basic)
+result_power = power_operations(a, b)
+print("Power Operation Result:", result_power)
+result_power_modulo = power_operations(a, b, modulo=c)
+print("Power Operation Result with modulo:", result_power_modulo)
+operations = [
+    (lambda x, y: x + y, (a, b))
+    , (lambda x, y: x * y, (a, b))
+]
+result_apply = apply_operations(operations)
+print("Apply operations result:", result_apply)
